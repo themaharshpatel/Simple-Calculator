852a612b3deab36552462c644fbca64beb428b5f
miral2590 <songhela@gmail.com>
2020-06-18 18:16:45 +0530

    Miral

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -4,16 +4,79 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    String inputText, resultText;
+    String operator, operand;
+    TextView resultTextView;
+    List<String> operators = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        resultTextView = findViewById(R.id.textView);
+        operators.add("+");
+        operators.add("-");
+        operators.add("*");
+        operators.add("/");
     }
 
-    public void btnClicked(View V){
+    public void onBtnClicked(View view){
+        inputText = view.getTag().toString();
+        resultText = resultTextView.getText().toString();
+
+        if(inputText.equals("AC"))
+        {
+            resultTextView.setText("0");
+        }
+        else if(operators.contains(inputText))
+        {
+            operand = resultText;
+            operator = inputText;
+            resultTextView.setText(inputText);
+        }
+        else if(inputText.matches("[0-9]+"))
+        {
+            if(resultText.equals("0") || operators.contains(resultText))
+            {
+                resultTextView.setText(inputText);
+            }
+            else
+            {
+                resultTextView.setText(resultText+inputText);
+            }
+        }
+        else if(inputText.equals("="))
+        {
+            int num1 = Integer.parseInt(operand);
+            int num2 = Integer.parseInt(resultText);
+
+            if(operator.equals("+"))
+            {
+                int ans = num1 + num2;
+                resultTextView.setText(String.valueOf(ans));
+            }
+            else if(operator.equals("-"))
+            {
+                int ans = num1 - num2;
+                resultTextView.setText(String.valueOf(ans));
+            }
+            else if(operator.equals("*"))
+            {
+                int ans = num1 * num2;
+                resultTextView.setText(String.valueOf(ans));
+            }
+            else if(operator.equals("/"))
+            {
+                int ans = num1 / num2;
+                resultTextView.setText(String.valueOf(ans));
+            }
+        }
 
 
     }
